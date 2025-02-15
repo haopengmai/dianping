dfe7e98f275e28cce7a35f541d7902b924eac9d3
lcy <1603639229@qq.com>
2025-02-16 01:42:47 +0800

    修改 Main 文件

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -11,6 +11,6 @@ public class HmDianPingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HmDianPingApplication.class, args);
-        System.out.println(1);
+        System.out.println("我是 master ");
     }
 }
